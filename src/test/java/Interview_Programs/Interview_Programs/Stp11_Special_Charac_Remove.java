f3d740a3e2c598ae26a1ce7b34a552b34487e394
pavan <seshasaim72@gmail.com>
2024-05-10 05:29:42 +0530

    First commit

--- src/test/java/Interview_Programs/Interview_Programs/Stp11_Special_Charac_Remove.java
+++ src/test/java/Interview_Programs/Interview_Programs/Stp11_Special_Charac_Remove.java
@@ -6,16 +6,24 @@ public class Stp11_Special_Charac_Remove {
 	
 	public static void main(String args[]) {
 		
-		String email="seshusai76@gmail.com";
+		String email="seshusai_76!#$%^,.     )(][/';@gmail.com";
 		String s1="0";
-		
+		System.out.println(email);
 //		email = email.replaceAll("[^a-zA-Z0-9]", " ");
-		email = email.replaceAll("[^A-Z0-9]", "");
+		email = email.replaceAll("[^a-zA-Z0-9_]", "");
+		System.out.println(email);
+		
+		if(email.contains("[^a-zA-Z0-9_]")) {
+			email.replaceAll("[^a-zA-Z0-9_]", "");
+			System.out.println(email);
+		}
+//		email = email.replaceAll(" ", "_");
+//		System.out.println(email);
 //		s1 = s1.replaceAll("[^A-Z0-9]", "");
 //		System.out.println(email);
 //		System.out.println(s1);
-		int s2=Integer.parseInt(s1)+Integer.parseInt(email);
-		System.out.println(s2);
+//		int s2=Integer.parseInt(s1)+Integer.parseInt(email);
+//		System.out.println(s2);
 	}
 
 }
